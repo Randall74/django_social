c8f3d751e852645155725789a4a2729c6aae20a6
Jason R. Hicks <jasonrandall74@gmail.com>
2023-12-20 07:42:49 -0500

    removed key

--- django_social/settings.py
+++ django_social/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@h6j^w!=w81@(*gg5o0(c)=6tc@niqx*p6y_gc&_&m6bbm=1ew'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
